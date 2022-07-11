d48b79a65434e4a5527d08e08d01cdefd8185e39
JWGardiner <jamesgardiner340@gmail.com>
2022-07-11 12:18:47 +0100

    Added a comment for clarification

--- src/main/java/com/jwg/frameapi/frameapi.java
+++ src/main/java/com/jwg/frameapi/frameapi.java
@@ -6,7 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class frameapi implements ModInitializer {
+	//More or less, everything in this file is not useful, and here for debugging purposes.
+	//In a real-world scenario, all that'd be in here is logging, and initializing everything.
+	//To test the API is working there may be blocks, items etc all in here; just remember:
+	//This is an API; it adds nothing on it's own and requires other mods to do anything.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Frame-API");
+
 
 	@Override
 	public void onInitialize(ModContainer mod) {
